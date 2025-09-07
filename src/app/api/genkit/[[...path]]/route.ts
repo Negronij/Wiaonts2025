5b2e36b5a7080184e734f99ba80fccc3d1e0eeb3
Negronij <juan.ignacio.negroni@gmail.com>
2025-09-07 20:58:43 -0300

    Update route.ts

--- src/app/api/genkit/[[...path]]/route.ts
+++ src/app/api/genkit/[[...path]]/route.ts
@@ -1,1 +1,9 @@
-export const { GET, POST } = defineNextHandler();
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  return NextResponse.json({ message: "API vacía" });
+}
+
+export async function POST(req: NextRequest) {
+  return NextResponse.json({ message: "API vacía" });
+}
